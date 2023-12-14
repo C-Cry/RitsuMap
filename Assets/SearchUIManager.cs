d53672944a99675d248fb7108588878c7be68d53
Faiq Sayyidan Setiawan <faiqsayyidan50304@gmail.com>
2023-12-15 05:13:34 +0900

    UI
    Search Bar update

--- Assets/SearchUIManager.cs
+++ Assets/SearchUIManager.cs
@@ -1,36 +1,20 @@
 using UnityEngine;
-using UnityEngine.EventSystems; // Required when using Event systems.
 
 public class SearchUIManager : MonoBehaviour
 {
-    public GameObject searchPanel; // Assign your search results panel here
-    public GameObject backgroundPanel; // Assign your new background panel here
+    public GameObject panel; // You will assign the Panel to this variable in the Inspector
 
-    void Start()
+    // Call this method to show the search results panel
+    public void ShowPanel()
     {
-        // Optionally initialize panels to be hidden
-        HidePanel();
+        if (panel != null)
+            panel.SetActive(true);
     }
 
-    public void TogglePanel()
-    {
-        // This will check the current active state of the searchPanel and reverse it
-        bool isActive = searchPanel.activeSelf;
-        searchPanel.SetActive(!isActive);
-        backgroundPanel.SetActive(!isActive); // The background panel follows the search panel's state
-    }
-
-    // This method is specifically for hiding both the background and search panels
+    // Optional: Call this method to hide the panel
     public void HidePanel()
     {
-        searchPanel.SetActive(false);
-        backgroundPanel.SetActive(false); // Ensure the background panel is also hidden
-    }
-
-    // Call this method when the background panel is clicked
-    public void OnBackgroundClicked()
-    {
-        // When the background is clicked, we want to ensure that we hide the panels
-        HidePanel();
+        if (panel != null)
+            panel.SetActive(false);
     }
 }
